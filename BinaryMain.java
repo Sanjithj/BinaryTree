087ca44bb7e33bc952697cfa5e76792ffdec9b72
Sanjithj <sanjithj591997@gmail.com>
2022-07-10 16:25:27 +0530

    Added: Binary Tree

---
+++ BinaryMain.java
@@ -0,0 +1,43 @@
+package com.bridgelabz;
+
+import com.bridgelabz.Node.BinaryNode;
+
+public class BinaryMain<K extends Comparable<K>> {
+		private BinaryNode<K> root;
+			public void add(K key)
+			{
+				this.root = this.insertData(root,key);
+			}
+
+			public BinaryNode<K> insertData(BinaryNode<K> current, K key)
+			{
+				if(current ==  null)
+					return null;
+				int compareResult = key.compareTo(current.key);
+				if(compareResult==0) return current;
+				if(compareResult<0)
+				{
+					current.left = insertData(current.left, key);
+				}
+				else 
+				{
+					current.right = insertData(current.right, key);
+				}
+				return current;
+			}
+			@Override
+			public String toString() 
+			{
+				return "MyBinary [root=" + root + "]";
+			}
+
+		
+			public static void main(String[] args)
+			{
+				BinaryMain<Integer> binaryTree = new BinaryMain<>();
+				binaryTree.add(56);
+				binaryTree.add(30);
+				binaryTree.add(70);
+				System.out.println(binaryTree+"\n");
+			}
+}
---
+++ Node.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+public class Node {
+	public class BinaryNode<T> {
+		T key;
+		BinaryNode<T> left;
+		BinaryNode<T> right;
+		
+		public BinaryNode(T key)
+		{
+			super();
+			this.key = key;
+			this.left = null;
+			this.right = null;
+		}
+	   @Override
+		public String toString() 
+		{
+			return "BinaryNode [data=" + key + ", left=" + left + ", right=" + right + "]";
+		}
+
+	}
+}
